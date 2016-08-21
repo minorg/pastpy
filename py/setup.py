c4e71535d180d0e1dd317548dae7372a3f873efd
minorg <minorg@server.fake>
2016-08-21 12:43:06 -0400

    start of command infrastructure

--- py/setup.py
+++ py/setup.py
@@ -3,9 +3,17 @@ import os.path
 
 from setuptools import setup, find_packages
 
+MY_DIR_PATH = os.path.abspath(os.path.dirname(__file__))
 
-with open(os.path.join(os.path.abspath(os.path.dirname(__file__)), 'README.rst'), encoding='utf-8') as f:
+with open(os.path.join(MY_DIR_PATH, 'README.rst'), encoding='utf-8') as f:
     long_description = f.read()
+
+data_dir_path = os.path.join(MY_DIR_PATH, 'data')
+data_files = []
+for root_dir_path, _, file_names in os.walk(data_dir_path):
+    for file_name in file_names:
+        data_files.append(os.path.relpath(os.path.join(root_dir_path, file_name), MY_DIR_PATH))
+
 
 setup(
     author='Minor Gordon',
@@ -41,39 +49,19 @@ setup(
     # What does your project relate to?
     keywords='pastperfect',
 
-    # You can just specify the packages manually here if your project is
-    # simple. Or you can use find_packages().
-    packages=find_packages(exclude=('test',)),
-
-    # Alternatively, if you want to distribute just a my_module.py, uncomment
-    # this:
-    #   py_modules=["my_module"],
-
-    # List run-time dependencies here.  These will be installed by pip when
-    # your project is installed. For an analysis of "install_requires" vs pip's
-    # requirements files see:
-    # https://packaging.python.org/en/latest/requirements.html
-    install_requires=['pystache'],
-
-    # If there are data files included in your packages that need to be
-    # installed, specify them here.  If using Python 2.6 or less, then these
-    # have to be included in MANIFEST.in as well.
-    package_data={
-        'sample': ['package_data.dat'],
+    packages=find_packages('src'),
+    package_dir = {'':'src'},
+    package_data = {
+        # If any package contains *.txt or *.rst files, include them:
+        'pastpy.commands.site_template': ['*.mustache'],
     },
-
-    # Although 'package_data' is the preferred approach, in some case you may
-    # need to place data files outside of your packages. See:
-    # http://docs.python.org/3.4/distutils/setupscript.html#installing-additional-files # noqa
-    # In this case, 'data_file' will be installed into '<sys.prefix>/my_data'
-    data_files=[('pastpy_data', ['data'])],
 
     # To provide executable scripts, use entry points in preference to the
     # "scripts" keyword. Entry points provide cross-platform support and allow
     # pip to create the appropriate form of executable for the target platform.
     entry_points={
         'console_scripts': [
-            'sample=sample:main',
+            'pastpy=pastpy.main:main',
         ],
     },
 )
