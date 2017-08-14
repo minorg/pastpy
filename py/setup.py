21f1b26c937bea059acfd92a008b64d2266d2518
Minor G <minorg@server.fake>
2017-08-14 19:31:44 -0400

    fix .gitignore

--- py/setup.py
+++ py/setup.py
@@ -30,7 +30,7 @@ setup(
         'Intended Audience :: Developers',
 
         # Pick your license as you wish (should match "license" above)
-        'License :: OSI Approved :: BSD',
+        'License :: OSI Approved :: BSD License',
 
         # Specify the Python versions you support here. In particular, ensure
         # that you indicate whether you support Python 2, Python 3 or both.
