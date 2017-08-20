89e7b5e4c80c68456421739727126f543eaa47f7
Minor Gordon <minorg@server.fake>
2017-08-19 20:18:59 -0400

    regenerate thrift

--- lib/thrift/devbin/generate_record_thrift.py
+++ lib/thrift/devbin/generate_record_thrift.py
@@ -7,10 +7,31 @@ THRIFT_DIR_PATH = os.path.join(os.path.dirname(
 
 
 class RecordField(object):
+    THRIFT_IMPORTS_BY_TYPE = {
+        'date_time.DateTime': 'include "thryft/native/date_time.thrift"',
+        'decimal.decimal': 'include "thryft/native/decimal.thrift"',
+    }
+
     # DBF type is C for char, et al.
     # http://dbfread.readthedocs.io/en/latest/field_types.html
     THRIFT_TYPE_BY_DBF_TYPE = {
-        'C': 'string'
+        # C	text	unicode string
+        'C': 'string',
+
+        # D	date	datetime.date or None
+        'D': 'date_time.DateTime',
+
+        # L	logical	True, False or None
+        'L': 'bool',
+
+        # M	memo	unicode string (memo), byte string (picture or object) or None
+        'M': 'string',
+
+        # N	numeric	int, float or None
+        'N': 'decimal.Decimal',
+
+        # T	time	datetime.datetime
+        'T': 'date_time.DateTime',
     }
 
     def __init__(self, dbf_field_info, name):
@@ -18,11 +39,14 @@ class RecordField(object):
         self.__name = name
 
         dbf_type = chr(dbf_field_info.field_type)
-        self.__thrift_type = self.THRIFT_TYPE_BY_DBF_TYPE[dbf_type]
-
-        # field_length = field_info.length
-        # field_decimal = field_info.decimal
-        # field_py_type = field_info.py_type
+        if dbf_type == 'N':
+            if dbf_field_info.decimal > 0:
+                thrift_type = 'decimal.Decimal'
+            else:
+                thrift_type = 'i32'
+        else:
+            thrift_type = self.THRIFT_TYPE_BY_DBF_TYPE[dbf_type]
+        self.__thrift_type = thrift_type
 
     def __eq__(self, other):
         if self.name != other.name:
@@ -34,6 +58,9 @@ class RecordField(object):
     @property
     def name(self):
         return self.__name
+
+    def thrift_import(self):
+        return self.THRIFT_IMPORTS_BY_TYPE.get(self.thrift_type, None)
 
     def thrift_repr(self):
         dbf_type = [chr(self.__dbf_field_info.field_type)]
@@ -54,6 +81,7 @@ class RecordField(object):
 
 def generate_record_thrift(union_dbf_file_paths, thrift_file_name):
     fields_by_name = {}
+    thrift_imports = []
     for dbf_file_path in union_dbf_file_paths:
         with dbf.Table(dbf_file_path) as table:
             for field_name in table.field_names:
@@ -62,6 +90,9 @@ def generate_record_thrift(union_dbf_file_paths, thrift_file_name):
                 if field_name in fields_by_name:
                     assert field == fields_by_name[field_name]
                 fields_by_name[field_name] = field
+                thrift_import = field.thrift_import()
+                if thrift_import is not None and thrift_import not in thrift_imports:
+                    thrift_imports.append(thrift_import)
 
     field_thrift_reprs = []
     for field_name in sorted(fields_by_name.keys()):
@@ -69,14 +100,20 @@ def generate_record_thrift(union_dbf_file_paths, thrift_file_name):
     field_thrift_reprs = "\n\n".join(field_thrift_reprs)
     thrift_file_path = os.path.join(THRIFT_DIR_PATH, thrift_file_name)
 
+    thrift_imports = "\n".join(sorted(thrift_imports))
+
     struct_name = ''.join(part.capitalize()
                           for part in os.path.splitext(thrift_file_name)[0].split('_'))
-    with open(thrift_file_path, 'w+') as thrift_file:
+    with open(thrift_file_path, 'w+b') as thrift_file:
         thrift_file.write(("""\
 namespace * pastpy.models
 
+%(thrift_imports)s
+
 struct %(struct_name)s {
-}""" % locals()).encode("ascii"))
+%(field_thrift_reprs)s
+}
+""" % locals()).replace("\r\n", "\n").encode("ascii"))
         print('wrote', thrift_file_path)
 
 
--- lib/thrift/src/pastpy/models/object_record.thrift
+++ lib/thrift/src/pastpy/models/object_record.thrift
@@ -1,648 +1,537 @@
 namespace * pastpy.models
 
-include "pastpy/models/cardinal_direction.thrift"
-include "pastpy/models/cat.thrift"
-include "pastpy/models/condition.thrift"
 include "thryft/native/date_time.thrift"
-include "thryft/native/decimal.thrift"
-include "thryft/native/variant.thrift"
 
 struct ObjectRecord {
-    // @validation {"blank": false, "minLength": 1}
     optional string accessno;
 
-    // @validation {"blank": false, "minLength": 1}
     optional string accessory;
 
     optional decimal.Decimal acqvalue;
 
-    // @validation {"blank": false, "minLength": 1}
     optional string age;
 
-    // @validation {"blank": false, "minLength": 1}
     optional string appnotes;
 
-    // @validation {"blank": false, "minLength": 1}
     optional string appraisor;
 
-    // @validation {"blank": false, "minLength": 1}
     optional string assemzone;
 
-    optional i32 bagno;
+    optional string bagno;
 
-    optional i32 boxno;
+    optional string boxno;
 
-    // @validation {"blank": false, "minLength": 1}
     optional string caption;
 
-    optional cat.Cat cat;
+    optional string cat;
 
-    // @validation {"blank": false, "minLength": 1}
     optional string catby;
 
     optional date_time.DateTime catdate;
 
-    // @validation {"blank": false, "minLength": 1}
     optional string cattype;
 
-    // @validation {"blank": false, "minLength": 1}
     optional string chemcomp;
 
-    // @validation {"minExclusive": 0}
     optional decimal.Decimal circum;
 
-    // @validation {"minExclusive": 0}
     optional decimal.Decimal circumft;
 
-    // @validation {"minExclusive": 0}
     optional decimal.Decimal circumin;
 
-    // @validation {"blank": false, "minLength": 1}
     optional string classes;
 
     optional date_time.DateTime colldate;
 
-    // @validation {"blank": false, "minLength": 1}
     optional string collection;
 
-    // @validation {"blank": false, "minLength": 1}
     optional string collector;
 
     optional date_time.DateTime conddate;
 
-    // @validation {"blank": false, "minLength": 1}
     optional string condexam;
 
-    optional condition.Condition condition;
+    optional string condition;
 
-    // @validation {"blank": false, "minLength": 1}
     optional string condnotes;
 
-    // @validation {"blank": false, "minLength": 1}
     optional string count;
 
-    // @validation {"blank": false, "minLength": 1}
     optional string creator;
 
-    // @validation {"blank": false, "minLength": 1}
     optional string creator2;
 
-    // @validation {"blank": false, "minLength": 1}
     optional string creator3;
 
-    // @validation {"blank": false, "minLength": 1}
     optional string credit;
 
-    // @validation {"blank": false, "minLength": 1}
     optional string crystal;
 
-    // @validation {"blank": false, "minLength": 1}
     optional string culture;
 
     optional decimal.Decimal curvalmax;
 
     optional decimal.Decimal curvalue;
 
-    // @validation {"blank": false, "minLength": 1}
     optional string dataset;
 
-    // @validation {"blank": false, "minLength": 1}
     optional string date;
 
-    // @validation {"blank": false, "minLength": 1}
     optional string datingmeth;
 
-    // @validation {"blank": false, "minLength": 1}
     optional string datum;
 
-    // @validation {"minExclusive": 0}
     optional decimal.Decimal depth;
 
-    // @validation {"minExclusive": 0}
     optional decimal.Decimal depthft;
 
-    // @validation {"minExclusive": 0}
     optional decimal.Decimal depthin;
 
-    // @validation {"blank": false, "minLength": 1}
     optional string descrip;
 
-    // @validation {"minExclusive": 0}
     optional decimal.Decimal diameter;
 
-    // @validation {"minExclusive": 0}
     optional decimal.Decimal diameterft;
 
-    // @validation {"minExclusive": 0}
     optional decimal.Decimal diameterin;
 
-    // @validation {"blank": false, "minLength": 1}
     optional string dimnotes;
 
     optional i32 dimtype;
 
-    // @validation {"blank": false, "minLength": 1}
     optional string dispvalue;
 
-    // @validation {"blank": false, "minLength": 1}
-    optional string earlydate;
+    optional i32 earlydate;
 
-    // @validation {"blank": false, "minLength": 1}
     optional string elements;
 
-    // @validation {"blank": false, "minLength": 1}
     optional string epoch;
 
-    // @validation {"blank": false, "minLength": 1}
     optional string era;
 
-    // @validation {"blank": false, "minLength": 1}
     optional string event;
 
-    optional cardinal_direction.CardinalDirection ew;
+    optional string ew;
 
     optional date_time.DateTime excavadate;
 
-    // @validation {"blank": false, "minLength": 1}
     optional string excavateby;
 
-    // @validation {"blank": false, "minLength": 1}
     optional string exhibitid;
 
     optional i32 exhibitno;
 
-    // @validation {"minLength": 1}
-    optional map<i32, string> exhlabel;
+    optional string exhlabel1;
 
-    // @validation {"blank": false, "minLength": 1}
-    optional string exhstart;
+    optional string exhlabel2;
 
-    // @validation {"blank": false, "minLength": 1}
+    optional string exhlabel3;
+
+    optional string exhlabel4;
+
+    optional date_time.DateTime exhstart;
+
     optional string family;
 
-    // @validation {"blank": false, "minLength": 1}
     optional string feature;
 
     optional date_time.DateTime flagdate;
 
-    // @validation {"blank": false, "minLength": 1}
     optional string flagnotes;
 
-    // @validation {"blank": false, "minLength": 1}
     optional string flagreason;
 
-    // @validation {"blank": false, "minLength": 1}
     optional string formation;
 
-    // @validation {"blank": false, "minLength": 1}
     optional string fossils;
 
-    // @validation {"blank": false, "minLength": 1}
     optional string found;
 
-    // @validation {"blank": false, "minLength": 1}
     optional string fracture;
 
-    // @validation {"blank": false, "minLength": 1}
     optional string frame;
 
-    // @validation {"blank": false, "minLength": 1}
     optional string framesize;
 
-    // @validation {"blank": false, "minLength": 1}
     optional string genus;
 
-    // @validation {"blank": false, "minLength": 1}
     optional string gparent;
 
-    // @validation {"blank": false, "minLength": 1}
     optional string grainsize;
 
-    // @validation {"blank": false, "minLength": 1}
     optional string habitat;
 
-    // @validation {"blank": false, "minLength": 1}
     optional string hardness;
 
-    // @validation {"minExclusive": 0}
     optional decimal.Decimal height;
 
-    // @validation {"minExclusive": 0}
     optional decimal.Decimal heightft;
 
-    // @validation {"minExclusive": 0}
     optional decimal.Decimal heightin;
 
-    // @validation {"blank": false, "minLength": 1}
     optional string homeloc;
 
-    // @validation {"blank": false, "minLength": 1}
     optional string idby;
 
     optional date_time.DateTime iddate;
 
-    // @validation {"blank": false, "minLength": 1}
     optional string imagefile;
 
     optional i32 imageno;
 
-    // @validation {"blank": false, "minLength": 1}
     optional string imagesize;
 
-    // @validation {"blank": false, "minLength": 1}
     optional string inscomp;
 
-    // @validation {"blank": false, "minLength": 1}
     optional string inscrlang;
 
-    // @validation {"blank": false, "minLength": 1}
     optional string inscrpos;
 
-    // @validation {"blank": false, "minLength": 1}
     optional string inscrtech;
 
-    // @validation {"blank": false, "minLength": 1}
     optional string inscrtext;
 
-    // @validation {"blank": false, "minLength": 1}
     optional string inscrtrans;
 
-    optional variant.Variant inscrtype;
+    optional string inscrtype;
 
     optional date_time.DateTime insdate;
 
-    // @validation {"blank": false, "minLength": 1}
     optional string insphone;
 
-    // @validation {"blank": false, "minLength": 1}
     optional string inspremium;
 
-    // @validation {"blank": false, "minLength": 1}
     optional string insrep;
 
     optional decimal.Decimal insvalue;
 
-    // @validation {"blank": false, "minLength": 1}
     optional string invnby;
 
     optional date_time.DateTime invndate;
 
-    // @validation {"blank": false, "minLength": 1}
     optional string kingdom;
 
-    // @validation {"minExclusive": 0}
     optional decimal.Decimal latdeg;
 
-    // @validation {"blank": false, "minLength": 1}
-    optional string latedate;
+    optional i32 latedate;
 
-    // @validation {"blank": false, "minLength": 1}
     optional string legal;
 
-    // @validation {"minExclusive": 0}
     optional decimal.Decimal length;
 
-    // @validation {"minExclusive": 0}
     optional decimal.Decimal lengthft;
 
-    // @validation {"minExclusive": 0}
     optional decimal.Decimal lengthin;
 
-    // @validation {"blank": false, "minLength": 1}
     optional string level;
 
-    // @validation {"blank": false, "minLength": 1}
     optional string lithofacie;
 
-    // @validation {"blank": false, "minLength": 1}
     optional string loancond;
 
-    // @validation {"blank": false, "minLength": 1}
-    optional string loandue;
+    optional date_time.DateTime loandue;
 
-    // @validation {"blank": false, "minLength": 1}
     optional string loanid;
 
-    // @validation {"blank": false, "minLength": 1}
     optional string loaninno;
 
     optional i32 loanno;
 
     optional date_time.DateTime loanrenew;
 
-    // @validation {"minLength": 1}
-    optional map<i32, string> locfield;
+    optional string locfield1;
 
-    // @validation {"minExclusive": 0}
+    optional string locfield2;
+
+    optional string locfield3;
+
+    optional string locfield4;
+
+    optional string locfield5;
+
+    optional string locfield6;
+
     optional decimal.Decimal longdeg;
 
-    // @validation {"blank": false, "minLength": 1}
     optional string luster;
 
-    // @validation {"blank": false, "minLength": 1}
     optional string made;
 
-    // @validation {"blank": false, "minLength": 1}
     optional string maintcycle;
 
     optional date_time.DateTime maintdate;
 
-    // @validation {"blank": false, "minLength": 1}
     optional string maintnote;
 
-    // @validation {"blank": false, "minLength": 1}
     optional string material;
 
-    // @validation {"blank": false, "minLength": 1}
     optional string medium;
 
-    // @validation {"blank": false, "minLength": 1}
     optional string member;
 
-    // @validation {"blank": false, "minLength": 1}
     optional string mmark;
 
-    // @validation {"blank": false, "minLength": 1}
     optional string nhclass;
 
-    // @validation {"blank": false, "minLength": 1}
     optional string nhorder;
 
-    // @validation {"blank": false, "minLength": 1}
     optional string notes;
 
-    optional cardinal_direction.CardinalDirection ns;
+    optional string ns;
 
-    // @validation {"blank": false, "minLength": 1}
     optional string objectid;
 
-    // @validation {"blank": false, "minLength": 1}
     optional string objname;
 
-    // @validation {"blank": false, "minLength": 1}
     optional string objname2;
 
-    // @validation {"blank": false, "minLength": 1}
     optional string objname3;
 
-    // @validation {"blank": false, "minLength": 1}
     optional string objnames;
 
-    // @validation {"blank": false, "minLength": 1}
     optional string occurrence;
 
-    // @validation {"blank": false, "minLength": 1}
     optional string oldno;
 
-    // @validation {"blank": false, "minLength": 1}
     optional string origin;
 
-    // @validation {"blank": false, "minLength": 1}
     optional string othername;
 
-    // @validation {"blank": false, "minLength": 1}
     optional string otherno;
 
     optional date_time.DateTime outdate;
 
-    // @validation {"blank": false, "minLength": 1}
     optional string owned;
 
-    // @validation {"blank": false, "minLength": 1}
     optional string parent;
 
-    // @validation {"blank": false, "minLength": 1}
     optional string people;
 
-    // @validation {"blank": false, "minLength": 1}
     optional string period;
 
-    // @validation {"blank": false, "minLength": 1}
     optional string phylum;
 
-    // @validation {"blank": false, "minLength": 1}
     optional string policyno;
 
-    // @validation {"blank": false, "minLength": 1}
     optional string ppid;
 
-    // @validation {"blank": false, "minLength": 1}
     optional string preparator;
 
     optional date_time.DateTime prepdate;
 
-    // @validation {"blank": false, "minLength": 1}
     optional string preserve;
 
-    // @validation {"blank": false, "minLength": 1}
     optional string pressure;
 
-    // @validation {"blank": false, "minLength": 1}
     optional string provenance;
 
-    // @validation {"blank": false, "minLength": 1}
     optional string pubnotes;
 
-    // @validation {"blank": false, "minLength": 1}
+    optional string qrurl;
+
     optional string recas;
 
-    optional date_time.DateTime recdate;
+    optional string recdate;
 
-    // @validation {"blank": false, "minLength": 1}
     optional string recfrom;
 
-    // @validation {"blank": false, "minLength": 1}
     optional string relation;
 
-    // @validation {"blank": false, "minLength": 1}
     optional string relnotes;
 
     optional date_time.DateTime renewuntil;
 
-    // @validation {"blank": false, "minLength": 1}
     optional string repatby;
 
-    // @validation {"blank": false, "minLength": 1}
     optional string repatclaim;
 
     optional date_time.DateTime repatdate;
 
-    // @validation {"blank": false, "minLength": 1}
     optional string repatdisp;
 
-    // @validation {"blank": false, "minLength": 1}
     optional string repathand;
 
-    // @validation {"blank": false, "minLength": 1}
     optional string repatnotes;
 
-    // @validation {"blank": false, "minLength": 1}
-    optional string repatnotic;
+    optional date_time.DateTime repatnotic;
 
-    optional i32 repattype;
+    optional string repattype;
 
-    // @validation {"blank": false, "minLength": 1}
     optional string rockclass;
 
-    // @validation {"blank": false, "minLength": 1}
     optional string rockcolor;
 
-    // @validation {"blank": false, "minLength": 1}
     optional string rockorigin;
 
-    optional i32 rocktype;
+    optional string rocktype;
 
-    // @validation {"blank": false, "minLength": 1}
     optional string role;
 
-    // @validation {"blank": false, "minLength": 1}
     optional string role2;
 
-    // @validation {"blank": false, "minLength": 1}
     optional string role3;
 
-    // @validation {"blank": false, "minLength": 1}
     optional string school;
 
-    // @validation {"blank": false, "minLength": 1}
     optional string sex;
 
-    // @validation {"blank": false, "minLength": 1}
+    optional string sgflag;
+
     optional string signedname;
 
-    // @validation {"blank": false, "minLength": 1}
     optional string signloc;
 
-    // @validation {"blank": false, "minLength": 1}
     optional string site;
 
-    // @validation {"blank": false, "minLength": 1}
     optional string siteno;
 
-    // @validation {"blank": false, "minLength": 1}
     optional string specgrav;
 
-    // @validation {"blank": false, "minLength": 1}
     optional string species;
 
-    // @validation {"blank": false, "minLength": 1}
     optional string sprocess;
 
-    // @validation {"blank": false, "minLength": 1}
     optional string stage;
 
-    // @validation {"blank": false, "minLength": 1}
     optional string status;
 
-    // @validation {"blank": false, "minLength": 1}
     optional string statusby;
 
     optional date_time.DateTime statusdate;
 
-    // @validation {"blank": false, "minLength": 1}
     optional string sterms;
 
-    // @validation {"blank": false, "minLength": 1}
     optional string stratum;
 
-    // @validation {"blank": false, "minLength": 1}
     optional string streak;
 
-    // @validation {"blank": false, "minLength": 1}
     optional string subfamily;
 
-    // @validation {"blank": false, "minLength": 1}
     optional string subjects;
 
-    // @validation {"blank": false, "minLength": 1}
     optional string subspecies;
 
-    // @validation {"blank": false, "minLength": 1}
     optional string technique;
 
-    // @validation {"blank": false, "minLength": 1}
     optional string tempauthor;
 
-    // @validation {"blank": false, "minLength": 1}
     optional string tempby;
 
     optional date_time.DateTime tempdate;
 
-    // @validation {"blank": false, "minLength": 1}
     optional string temperatur;
 
-    // @validation {"blank": false, "minLength": 1}
     optional string temploc;
 
-    // @validation {"blank": false, "minLength": 1}
     optional string tempnotes;
 
-    // @validation {"blank": false, "minLength": 1}
     optional string tempreason;
 
-    // @validation {"blank": false, "minLength": 1}
     optional string tempuntil;
 
-    // @validation {"blank": false, "minLength": 1}
     optional string texture;
 
-    // @validation {"blank": false, "minLength": 1}
     optional string title;
 
-    // @validation {"minLength": 1}
-    optional map<i32, string> tlocfield;
+    optional string tlocfield1;
 
-     // @validation {"minLength": 1}
-    optional map<i32, variant.Variant> udf;
+    optional string tlocfield2;
 
-    // @validation {"blank": false, "minLength": 1}
+    optional string tlocfield3;
+
+    optional string tlocfield4;
+
+    optional string tlocfield5;
+
+    optional string tlocfield6;
+
+    optional string udf1;
+
+    optional string udf10;
+
+    optional string udf11;
+
+    optional string udf12;
+
+    optional i32 udf13;
+
+    optional decimal.Decimal udf14;
+
+    optional decimal.Decimal udf15;
+
+    optional decimal.Decimal udf16;
+
+    optional decimal.Decimal udf17;
+
+    optional date_time.DateTime udf18;
+
+    optional date_time.DateTime udf19;
+
+    optional string udf2;
+
+    optional date_time.DateTime udf20;
+
+    optional string udf21;
+
+    optional string udf22;
+
+    optional string udf3;
+
+    optional string udf4;
+
+    optional string udf5;
+
+    optional string udf6;
+
+    optional string udf7;
+
+    optional string udf8;
+
+    optional string udf9;
+
     optional string unit;
 
     optional date_time.DateTime updated;
 
-    // @validation {"blank": false, "minLength": 1}
     optional string updatedby;
 
-    // @validation {"blank": false, "minLength": 1}
     optional string used;
 
     optional date_time.DateTime valuedate;
 
-    // @validation {"blank": false, "minLength": 1}
     optional string varieties;
+
+    optional string vexhtml;
+
+    optional string vexlabel1;
+
+    optional string vexlabel2;
+
+    optional string vexlabel3;
+
+    optional string vexlabel4;
 
     optional bool webinclude;
 
-    // @validation {"minExclusive": 0}
     optional decimal.Decimal weight;
 
-    // @validation {"minExclusive": 0}
     optional decimal.Decimal weightin;
 
-    // @validation {"minExclusive": 0}
     optional decimal.Decimal weightlb;
 
-    // @validation {"minExclusive": 0}
     optional decimal.Decimal width;
 
-    // @validation {"minExclusive": 0}
     optional decimal.Decimal widthft;
 
-    // @validation {"minExclusive": 0}
     optional decimal.Decimal widthin;
 
-    // @validation {"blank": false, "minLength": 1}
-    optional string xcord;
+    optional decimal.Decimal xcord;
 
-    // @validation {"blank": false, "minLength": 1}
-    optional string ycord;
+    optional decimal.Decimal ycord;
 
-    // @validation {"blank": false, "minLength": 1}
-    optional string zcord;
+    optional decimal.Decimal zcord;
 
-    // @validation {"blank": false, "minLength": 1}
     optional string zsorter;
 
-    // @validation {"blank": false, "minLength": 1}
     optional string zsorterx;
 }
