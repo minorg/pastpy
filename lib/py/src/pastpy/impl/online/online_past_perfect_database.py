dd83d0a25ad4d18733d046373396b26789feac94
Minor G <minorg@server.fake>
2018-03-17 10:02:46 -0500

    refactor downloader

--- lib/py/src/pastpy/impl/online/online_past_perfect_database.py
+++ lib/py/src/pastpy/impl/online/online_past_perfect_database.py
@@ -5,43 +5,74 @@ import logging
 
 
 class OnlinePastPerfectDatabase(object):
-    def __init__(self, name, dir_path=None):
+    class _Downloader(object):
+        def __init__(self, *, file_paths, host):
+            self.__http_client_connection = None
+            self.__file_paths = file_paths
+            self.__host = host
+
+        def download_objects_list(self):
+            objects_list_dir_path = self.__file_paths.objects_list_dir_path
+            if not os.path.isdir(objects_list_dir_path):
+                os.makedirs(objects_list_dir_path)
+                logging.info("created directory %s", objects_list_dir_path)
+
+            page_i = 1
+            while self.__download_objects_list_page(page_i):
+                page_i = page_i + 1
+
+        def __download_objects_list_page(self, page_i):
+            page_file_path = self.__file_paths.objects_list_page_file_path(page_i)
+            if os.path.isfile(page_file_path):
+                logging.info("page file %s already exists, skipping", page_file_path)
+                page_i = page_i + 1
+                return True
+            page_url_path = "/webobject?page=" + str(page_i)
+            logging.info("retrieving %s", page_url_path)
+            self.__http_client_connection.request("GET", page_url_path)
+            response = self.__http_client_connection.getresponse()
+            page_contents = response.read()
+            logging.info("retrieved %s", page_url_path)
+            if "No results found" in str(page_contents):
+                logging.info("page %d was the last, stopping", page_i)
+                return False
+            with open(page_file_path, "w+b") as page_file:
+                page_file.write(page_contents)
+                logging.info("wrote %s", page_file_path)
+            return True
+
+        def __enter__(self):
+            if not os.path.isdir(self.__file_paths.root_dir_path):
+                os.makedirs(self.__file_paths.root_dir_path)
+
+            self.__http_client_connection = http.client.HTTPConnection(self.__host)
+
+            return self
+
+        def __exit__(self, *args, **kwds):
+            self.__http_client_connection.close()
+
+    class _FilePaths(object):
+        def __init__(self, root_dir_path):
+            self.__root_dir_path = root_dir_path
+
+        @property
+        def objects_list_dir_path(self):
+            return os.path.join(self.__root_dir_path, "objects", "list")
+
+        def objects_list_page_file_path(self, page_i):
+            return os.path.join(self.objects_list_dir_path, str(page_i) + ".html")
+
+        @property
+        def root_dir_path(self):
+            return self.__root_dir_path
+
+    def __init__(self, *, name, dir_path=None):
         self.__name = name
         if dir_path is None:
             dir_path = os.path.abspath(self.__name)
-        self.__dir_path = dir_path
+        self.__file_paths = OnlinePastPerfectDatabase._FilePaths(dir_path)
 
     def download(self):
-        if not os.path.isdir(self.__dir_path):
-            os.makedirs(self.__dir_path)
-        http_client_connection = http.client.HTTPConnection(self.__name + ".pastperfectonline.com")
-        try:
-            webobject_url_path = "/webobject"
-            page_i = 1
-            pages_dir_path = os.path.join(self.__dir_path, "objects", "list")
-            if not os.path.isdir(pages_dir_path):
-                os.makedirs(pages_dir_path)
-                logging.info("created directory %s", pages_dir_path)
-            while True:
-                page_file_path = os.path.join(pages_dir_path, str(page_i) + ".html")
-                if os.path.isfile(page_file_path):
-                    logging.info("page file %s already exists, skipping", page_file_path)
-                    page_i = page_i + 1
-                    continue
-                page_url_path = webobject_url_path + "?page=" + str(page_i)
-                logging.info("retrieving %s", page_url_path)
-                http_client_connection.request("GET", page_url_path)
-                response = http_client_connection.getresponse()
-                page_contents = response.read()
-                logging.info("retrieved %s", page_url_path)
-                if "No results found" in str(page_contents):
-                    logging.info("page %d was the last, stopping", page_i)
-                    break
-                with open(page_file_path, "w+b") as page_file:
-                    page_file.write(page_contents)
-                    logging.info("wrote %s", page_file_path)
-                sleep(1)
-                page_i = page_i + 1
-                # break
-        finally:
-            http_client_connection.close()
+        with OnlinePastPerfectDatabase._Downloader(file_paths=self.__file_paths, host=self.__name + ".pastperfectonline.com") as downloader:
+            downloader.download_objects_list()
