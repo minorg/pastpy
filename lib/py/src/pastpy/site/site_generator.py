cbd50e0b6197fd9a4ded3dbe900175cf6960b6b2
Minor G <minorg@server.fake>
2018-04-07 14:17:47 -0500

    separate SiteObjectsReader

--- lib/py/src/pastpy/site/site_generator.py
+++ lib/py/src/pastpy/site/site_generator.py
@@ -1,25 +1,18 @@
 from collections import OrderedDict
 from datetime import date, datetime
-import json
 import logging
 import os.path
-import re
 import shutil
 import pystache
-import inspect
-from urllib.parse import urlparse
-from xml.sax import saxutils
-from pastpy.gen.site.site_attribute import SiteAttribute
 from pastpy.gen.site.site_configuration import SiteConfiguration
-from pastpy.gen.site.site_image import SiteImage
 from pastpy.gen.site.site_index import SiteIndex
 from pastpy.gen.site.site_metadata import SiteMetadata
 from pastpy.gen.site.site_nav_items import SiteNavItems
-from pastpy.gen.site.site_object import SiteObject
 from pastpy.gen.site.site_object_detail import SiteObjectDetail
 from pastpy.gen.site.site_objects_list import SiteObjectsList
 from pastpy.gen.site.site_objects_list_page_number import SiteObjectsListPageNumber
 from pastpy.gen.site.site_sitemap import SiteSitemap
+from pastpy.site.site_objects_reader import SiteObjectsReader
 
 
 class SiteGenerator(object):
@@ -97,7 +90,7 @@ class SiteGenerator(object):
 
         self.__copy_static_files()
 
-        self.__read_objects()
+        self.__objects = SiteObjectsReader(configuration=self.__configuration, database=self.__database).read_objects()
 
         self.__render_index()
         self.__render_object_details()
@@ -150,107 +143,6 @@ class SiteGenerator(object):
         return range(start, end + 1)
 
     def __read_objects(self):
-        out_images_dir_path = os.path.join(
-            self.__configuration.output_dir_path, 'img')
-        if not os.path.isdir(out_images_dir_path):
-            os.makedirs(out_images_dir_path)
-            self.__logger.debug(
-                "created output images directory %s", out_images_dir_path)
-
-        object_ids_by_file_name = {}
-        self.__objects = []
-        for database_object in self.__database.objects():
-            images = []
-            for database_image in database_object.images:
-                full_size_url = database_image.full_size_url
-                if full_size_url:
-                    full_size_url_parsed = urlparse(full_size_url)
-                    if full_size_url_parsed.scheme == "file":
-                        raise NotImplementedError(
-                            "TODO: rewrite the actual image object with a substitute")
-                        # full_size_file_path = unquote(full_size_url)[7:]
-                        # full_size_file_name = os.path.split(full_size_file_path)[1]
-                        # out_image_file_path = os.path.join(
-                        #     out_images_dir_path, full_size_file_name)
-                        # shutil.copyfile(full_size_file_path,
-                        #                 full_size_file_name)
-                        # self.__logger.debug(
-                        #     "copied %s to %s", full_size_file_path, out_image_file_path)
-                        # img_srcs.append('img/' + full_size_file_name)
-                images.append(SiteImage(
-                    full_size_url=database_image.full_size_url,
-                    thumbnail_url=database_image.thumbnail_url,
-                    title=database_image.title
-                ))
-
-            file_name = self.__to_valid_filename(database_object.id) + ".html"
-            existing_object_id = object_ids_by_file_name.get(file_name)
-            if existing_object_id is not None:
-                raise RuntimeError("two objects (%s and %s) map to the same file name (%s)" % (
-                    existing_object_id, database_object.id, file_name))
-            object_ids_by_file_name[file_name] = database_object.id
-
-            object_builder = SiteObject.builder()
-
-            object_builder.absolute_href = "/objects/details/" + file_name
-            object_builder.file_name = file_name
-
-            object_builder.full_size_images = \
-                tuple(image for image in images if image.full_size_url)
-            object_builder.thumbnail_images = \
-                tuple(image for image in images if image.thumbnail_url)
-            object_builder.has_full_size_images = len(
-                object_builder.full_size_images) > 0
-            object_builder.has_thumbnail_images = len(
-                object_builder.thumbnail_images) > 0
-            if object_builder.thumbnail_images:
-                object_builder.thumbnail_url = object_builder.thumbnail_images[0].thumbnail_url
-            else:
-                object_builder.thumbnail_url = "http://via.placeholder.com/210x211?text=Missing%20image"
-
-            object_builder.impl_attributes_list = \
-                tuple(SiteAttribute(name=name, value=value)
-                      for name, value in database_object.impl_attributes.items())
-
-            if database_object.name:
-                object_builder.name = database_object.name
-            elif database_object.othername:
-                object_builder.name = database_object.othername
-            else:
-                object_builder.name = database_object.id
-
-            standard_attributes_map = {}
-            standard_attributes_map_json = {}
-            standard_attributes_map_xml = {}
-            for member_name, member in inspect.getmembers(database_object):
-                if inspect.ismethod(member):
-                    continue
-                if member_name.startswith('_'):
-                    continue
-                elif member_name in ("images", "impl_attributes"):
-                    continue
-                value = getattr(database_object, member_name)
-                if value is not None:
-                    standard_attributes_map[member_name] = value
-                    standard_attributes_map_json[member_name] = json.dumps(value)
-                    standard_attributes_map_xml[member_name] = saxutils.escape(value)
-                else:
-                    standard_attributes_map_json[member_name] = ''
-                    standard_attributes_map_xml[member_name] = ''
-            object_builder.standard_attributes_list = \
-                tuple(SiteAttribute(name=name, value=value)
-                      for name, value in standard_attributes_map.items())
-            object_builder.standard_attributes_list_xml = tuple(SiteAttribute(key, value) for key, value in standard_attributes_map_xml.items())
-            object_builder.standard_attributes_map_json = standard_attributes_map_json
-            for name, value in standard_attributes_map.items():
-                setattr(object_builder, name, value)
-
-            if object_builder.title is None:
-                object_builder.title = object_builder.name
-
-            self.__objects.append(object_builder.build())
-            if len(self.__objects) == 100:
-                break
         self.__objects = tuple(self.__objects)
 
     def __render_file(self, *, context, file_name, out_file_relpath=None):
@@ -374,8 +266,3 @@ class SiteGenerator(object):
         if os.path.isdir(dir_path):
             shutil.rmtree(dir_path)
             self.__logger.debug("deleted directory %s", dir_path)
-
-    @staticmethod
-    def __to_valid_filename(s):
-        s = str(s).strip().replace(' ', '_')
-        return re.sub(r'(?u)[^-\w.]', '', s)
---
+++ lib/py/src/pastpy/site/site_objects_reader.py
@@ -0,0 +1,127 @@
+import inspect
+import json
+import logging
+import os.path
+import re
+from urllib.parse import urlparse
+from xml.sax import saxutils
+
+from pastpy.gen.site.site_attribute import SiteAttribute
+from pastpy.gen.site.site_image import SiteImage
+from pastpy.gen.site.site_object import SiteObject
+
+
+class SiteObjectsReader(object):
+    def __init__(self, configuration, database):
+        self.__configuration = configuration
+        self.__database = database
+        self.__logger = logging.getLogger(SiteObjectsReader.__class__.__name__)
+
+    def read_objects(self):
+        out_images_dir_path = os.path.join(
+            self.__configuration.output_dir_path, 'img')
+        if not os.path.isdir(out_images_dir_path):
+            os.makedirs(out_images_dir_path)
+            self.__logger.debug(
+                "created output images directory %s", out_images_dir_path)
+
+        object_ids_by_file_name = {}
+        objects = []
+        for database_object in self.__database.objects():
+            images = []
+            for database_image in database_object.images:
+                full_size_url = database_image.full_size_url
+                if full_size_url:
+                    full_size_url_parsed = urlparse(full_size_url)
+                    if full_size_url_parsed.scheme == "file":
+                        raise NotImplementedError(
+                            "TODO: rewrite the actual image object with a substitute")
+                        # full_size_file_path = unquote(full_size_url)[7:]
+                        # full_size_file_name = os.path.split(full_size_file_path)[1]
+                        # out_image_file_path = os.path.join(
+                        #     out_images_dir_path, full_size_file_name)
+                        # shutil.copyfile(full_size_file_path,
+                        #                 full_size_file_name)
+                        # self.__logger.debug(
+                        #     "copied %s to %s", full_size_file_path, out_image_file_path)
+                        # img_srcs.append('img/' + full_size_file_name)
+                images.append(SiteImage(
+                    full_size_url=database_image.full_size_url,
+                    thumbnail_url=database_image.thumbnail_url,
+                    title=database_image.title
+                ))
+
+            file_name = self.__to_valid_filename(database_object.id) + ".html"
+            existing_object_id = object_ids_by_file_name.get(file_name)
+            if existing_object_id is not None:
+                raise RuntimeError("two objects (%s and %s) map to the same file name (%s)" % (
+                    existing_object_id, database_object.id, file_name))
+            object_ids_by_file_name[file_name] = database_object.id
+
+            object_builder = SiteObject.builder()
+
+            object_builder.absolute_href = "/objects/details/" + file_name
+            object_builder.file_name = file_name
+
+            object_builder.full_size_images = \
+                tuple(image for image in images if image.full_size_url)
+            object_builder.thumbnail_images = \
+                tuple(image for image in images if image.thumbnail_url)
+            object_builder.has_full_size_images = len(
+                object_builder.full_size_images) > 0
+            object_builder.has_thumbnail_images = len(
+                object_builder.thumbnail_images) > 0
+            if object_builder.thumbnail_images:
+                object_builder.thumbnail_url = object_builder.thumbnail_images[0].thumbnail_url
+            else:
+                object_builder.thumbnail_url = "http://via.placeholder.com/210x211?text=Missing%20image"
+
+            object_builder.impl_attributes_list = \
+                tuple(SiteAttribute(name=name, value=value)
+                      for name, value in database_object.impl_attributes.items())
+
+            if database_object.name:
+                object_builder.name = database_object.name
+            elif database_object.othername:
+                object_builder.name = database_object.othername
+            else:
+                object_builder.name = database_object.id
+
+            standard_attributes_map = {}
+            standard_attributes_map_json = {}
+            standard_attributes_map_xml = {}
+            for member_name, member in inspect.getmembers(database_object):
+                if inspect.ismethod(member):
+                    continue
+                if member_name.startswith('_'):
+                    continue
+                elif member_name in ("images", "impl_attributes"):
+                    continue
+                value = getattr(database_object, member_name)
+                if value is not None:
+                    standard_attributes_map[member_name] = value
+                    standard_attributes_map_json[member_name] = json.dumps(value)
+                    standard_attributes_map_xml[member_name] = saxutils.escape(value)
+                else:
+                    standard_attributes_map_json[member_name] = ''
+                    standard_attributes_map_xml[member_name] = ''
+            object_builder.standard_attributes_list = \
+                tuple(SiteAttribute(name=name, value=value)
+                      for name, value in standard_attributes_map.items())
+            object_builder.standard_attributes_list_xml = tuple(SiteAttribute(key, value) for key, value in standard_attributes_map_xml.items())
+            object_builder.standard_attributes_map_json = standard_attributes_map_json
+            for name, value in standard_attributes_map.items():
+                setattr(object_builder, name, value)
+
+            if object_builder.title is None:
+                object_builder.title = object_builder.name
+
+            objects.append(object_builder.build())
+            if len(objects) == 100:
+                break
+        return objects
+
+    @staticmethod
+    def __to_valid_filename(s):
+        s = str(s).strip().replace(' ', '_')
+        return re.sub(r'(?u)[^-\w.]', '', s)
