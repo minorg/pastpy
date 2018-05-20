ae2dae7028a44f0e0069dd4906a2c0e7088cccea
Minor G <minorg@server.fake>
2018-05-20 17:54:45 -0500

    search functionality in JS

--- lib/py/src/pastpy/site/template/js/objects_js.py
+++ lib/py/src/pastpy/site/template/js/objects_js.py
@@ -9,8 +9,14 @@ class ObjectsJs(_Template):
         self.__objects = objects
 
     def render(self):
+        objects_json = {}
+        for object_ in self.__objects:
+            object_json = object_.standard_attributes_map.copy()
+            object_json["absolute_href"] = object_.absolute_href
+            object_json["thumbnail_url"] = object_.thumbnail_url
+            objects_json[object_.id] = object_json
         self._render_mustache(
-            context={"objects_json": json.dumps([object_.standard_attributes_map for object_ in self.__objects])},
+            context={"objects_json": json.dumps(objects_json)},
             out_file_relpath=os.path.join("js", "objects.js"),
             template_name='js/objects.js'
         )
