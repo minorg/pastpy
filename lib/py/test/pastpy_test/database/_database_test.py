ebf3daf269c36a1f731716f96d15abad1c40ff1e
Minor G <minorg@server.fake>
2018-12-08 09:40:46 -0600

    unittest.cfg

--- lib/py/test/pastpy_test/database/_database_test.py
+++ lib/py/test/pastpy_test/database/_database_test.py
@@ -2,7 +2,14 @@ import unittest
 
 
 class _DatabaseTest(unittest.TestCase):
+    def __init__(self, *args, **kwds):
+        unittest.TestCase.__init__(self, *args, **kwds)
+        self._database = None
+
     def test_objects(self):
+        if self._database is None:
+            return
+
         for object_ in self._database.objects():
             return
         self.fail()
