4d6ab1db751ce78ec5b04b46782d4c5e97a43b0a
Minor G <minorg@server.fake>
2018-12-14 19:55:38 -0600

    fix unit test

--- py/test/pastpy_test/database/test_database_configurations.py
+++ py/test/pastpy_test/database/test_database_configurations.py
@@ -5,9 +5,9 @@ from pastpy.gen.database.impl.online.online_database_configuration import Online
 
 class TestDatabaseConfigurations(object):
     DBF = DbfDatabaseConfiguration.builder()\
-        .set_pp_objects_dbf_file_path(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..', '..', '..', '..', 'devdata', 'buffalostate', 'ftt', 'PPSdata_Objects.dbf')))\
+        .set_pp_objects_dbf_file_path(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..', '..', '..', 'devdata', 'buffalostate', 'ftt', 'PPSdata_Objects.dbf')))\
         .set_pp_images_dir_path(".").build()
 
     ONLINE = OnlineDatabaseConfiguration.builder()\
-        .set_download_dir_path(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..', '..', '..', '..', 'devdata', 'tcmuseum')))\
+        .set_download_dir_path(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..', '..', '..', 'devdata', 'tcmuseum')))\
         .set_collection_name('Test').build()
