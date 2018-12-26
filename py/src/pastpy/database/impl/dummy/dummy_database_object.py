ebf8585b2c435f92dbfde8c8ee673bfbfcab8b0f
Minor G <minorg@server.fake>
2018-12-25 18:10:51 -0600

    fix DummyDatabaseObject

--- py/src/pastpy/database/impl/dummy/dummy_database_object.py
+++ py/src/pastpy/database/impl/dummy/dummy_database_object.py
@@ -24,6 +24,7 @@ class DummyDatabaseObject(DatabaseObject):
         for i in range(self.__configuration.images_per_object):
             yield DummyDatabaseImage(image_index=i, object_index=self.__index)
 
+    @property
     def impl_attributes(self):
         return {}
 
