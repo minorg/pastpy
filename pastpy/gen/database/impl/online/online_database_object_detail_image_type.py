8c045ee8d1708597aae37bea895bc769f8253c74
Minor G <minorg@server.fake>
2020-08-15 15:33:27 -0400

    convert remaining models from Thryft to NamedTuple

--- pastpy/gen/database/impl/online/online_database_object_detail_image_type.py
+++ pastpy/gen/database/impl/online/online_database_object_detail_image_type.py
@@ -1,32 +1,6 @@
-class OnlineDatabaseObjectDetailImageType(object):
-    INDIVIDUAL = None
-    LARGE = None
+from enum import Enum
 
-    def __init__(self, name, value):
-        object.__init__(self)
-        self.__name = name
-        self.__value = value
 
-    def __int__(self):
-        return self.__value
-
-    def __repr__(self):
-        return self.__name
-
-    def __str__(self):
-        return self.__name
-
-    @classmethod
-    def value_of(cls, name):
-        if name == 'INDIVIDUAL' or name == '0':
-            return getattr(OnlineDatabaseObjectDetailImageType, 'INDIVIDUAL')
-        elif name == 'LARGE' or name == '1':
-            return getattr(OnlineDatabaseObjectDetailImageType, 'LARGE')
-        raise ValueError(name)
-
-    @classmethod
-    def values(cls):
-        return (OnlineDatabaseObjectDetailImageType.INDIVIDUAL, OnlineDatabaseObjectDetailImageType.LARGE,)
-
-OnlineDatabaseObjectDetailImageType.INDIVIDUAL = OnlineDatabaseObjectDetailImageType('INDIVIDUAL', 0)
-OnlineDatabaseObjectDetailImageType.LARGE = OnlineDatabaseObjectDetailImageType('LARGE', 1)
+class OnlineDatabaseObjectDetailImageType(Enum):
+    INDIVIDUAL = "INDIVIDUAL"  # 0
+    LARGE = "LARGE"  # 1
