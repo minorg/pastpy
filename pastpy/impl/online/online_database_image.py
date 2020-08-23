f690a901b52b6301c146583fe081c32b2fe5de22
Minor G <minorg@server.fake>
2020-08-23 11:07:05 -0400

    fix online database image

--- pastpy/impl/online/online_database_image.py
+++ pastpy/impl/online/online_database_image.py
@@ -1,13 +1,26 @@
+from typing import Optional
+
 from pastpy.database_image import DatabaseImage
-from pastpy.impl.online.online_database_object_detail_image import OnlineDatabaseObjectDetailImage
-from pastpy.impl.online.online_database_objects_list_item import OnlineDatabaseObjectsListItem
+from pastpy.impl.online.online_database_object_detail_image import (
+    OnlineDatabaseObjectDetailImage,
+)
+from pastpy.impl.online.online_database_objects_list_item import (
+    OnlineDatabaseObjectsListItem,
+)
 
 
 class OnlineDatabaseImage(DatabaseImage):
-    def __init__(self, *, detail_image=None, list_item=None):
+    def __init__(
+        self,
+        *,
+        detail_image: Optional[OnlineDatabaseObjectDetailImage] = None,
+        list_item: Optional[OnlineDatabaseObjectsListItem] = None
+    ):
         assert (detail_image is not None) ^ (list_item is not None)
         self.__detail_image = detail_image
-        assert detail_image is None or isinstance(detail_image, OnlineDatabaseObjectDetailImage)
+        assert detail_image is None or isinstance(
+            detail_image, OnlineDatabaseObjectDetailImage
+        )
         self.__list_item = list_item
         assert list_item is None or isinstance(list_item, OnlineDatabaseObjectsListItem)
 
